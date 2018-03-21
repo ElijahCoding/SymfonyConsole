9abef794e1123d7b0df44bcc1267ea09de8d7d23
Elijah <elijahsharper1@gmail.com>
2018-03-21 16:22:28 +0300

    Database table seeding

--- app/Commands/UserSeederCommand.php
+++ app/Commands/UserSeederCommand.php
@@ -28,14 +28,29 @@ class UserSeederCommand extends Command
   {
     $this->setName('seed:users')
          ->setDescription('Seed users')
-         ->setHelp('This command will seed users in your database.');
+         ->setHelp('This command will seed users in your database.')
+         ->addOption('count', 'c', InputOption::VALUE_REQUIRED, 'How many fake users would you like to generate?', 1);
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     // get the count
+    $count = $input->getOption('count');
+
     // notify users that seeding has started
+    $output->writeln('<info>Seeding ' . $count . ' users</info>');
+
     // loop and generate fake records
+    for ($i = 0; $i < $count; $i++) {
+      $statement = $this->db->prepare("INSERT INTO users (name, email) VALUES (:name, :email)");
+
+      $statement->execute([
+        'name' => $this->faker->name,
+        'email' => $this->faker->email
+      ]);
+    }
+
     // notify user on completion
+    $output->writeln('<info>Finished seeding users</info>');
   }
 }
