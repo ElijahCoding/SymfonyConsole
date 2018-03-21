1bf53914c19182a120f67851d0d3ec48ad2785c4
Elijah <elijahsharper1@gmail.com>
2018-03-21 21:35:32 +0300

    structure built up

---
+++ Craft/bootstrap/app.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+  'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
+
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
+        'cache' => false
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
+};
